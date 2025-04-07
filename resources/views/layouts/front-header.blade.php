c13ac51735c7bdafc1288f872c70a2fcc4329889
Sibi haseb <abdul.haseeb.ali@gmail.com>
2025-04-08 02:52:27 +0500

    Changes in Header

---
+++ resources/views/layouts/front-header.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Saud-Aslam</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/macy@2"></script>
+    <link rel="stylesheet" href="{{ asset('asset/css/landing.css') }}">
+    @yield('styles')
+</head>
+
+<body>
+    <div class="fixed inset-y-0 left-0 w-12 bg-white z-50 white-bar-container">
+        <!-- Bottom Section: Hicham Habchi -->
+        <div class="text-2xl font-bold pb-4" style="margin-top: 50px; margin-left: 10px">
+            <a href="/" class="white-bar text-black sidebar-text">Saud Aslam</a>
+        </div>
+        <!-- Top Section: MENU and Hamburger -->
+        <div class="flex flex-col items-center pt-4" style="margin-top: 250px">
+            <button id="hamburger" class="p-2 focus:outline-none flex flex-col items-center">
+                <span class="white-bar text-black text-sm font-medium mb-4">MENU</span>
+                <svg viewBox="0 0 24 24" class="w-6 h-6 text-black transition-colors duration-300" fill="none"
+                    xmlns="http://www.w3.org/2000/svg" transform="rotate(90)">
+                    <g>
+                        <path d="M4 18L20 18" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path>
+                        <path d="M4 12L20 12" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path>
+                        <path d="M4 6L20 6" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path>
+                    </g>
+                </svg>
+            </button>
+        </div>
+    </div>
+    <div id="sidebar" class="fixed inset-y-0 left-12 w-64 sidebar-bg text-white shadow-lg z-40 text-center">
+        <div class="flex flex-col h-full p-6">
+            <!-- Menu Items -->
+            <nav class="flex flex-col space-y-2">
+                <a href="/" class="py-2 px-4 text-lg hover:bg-gray-800 rounded transition duration-200">Home
+                    page</a>
+                <a href="#" class="py-2 px-4 text-lg hover:bg-gray-800 rounded transition duration-200">Resume</a>
+                <a href="#"
+                    class="py-2 px-4 text-lg hover:bg-gray-800 rounded transition duration-200">Portfolio</a>
+                <a href="#"
+                    class="py-2 px-4 text-lg hover:bg-gray-800 rounded transition duration-200">Instagram</a>
+                <a href="#"
+                    class="py-2 px-4 text-lg hover:bg-gray-800 rounded transition duration-200">Artbook</a>
+                <a href="#" class="py-2 px-4 text-lg hover:bg-gray-800 rounded transition duration-200">Prints</a>
+                <a href="#"
+                    class="py-2 px-4 text-lg hover:bg-gray-800 rounded transition duration-200">Contact</a>
+            </nav>
+        </div>
+    </div>
+    @yield('content')
+    <script>
+        var macy = Macy({
+            container: '#js-macy',
+            trueOrder: false,
+            waitForImages: true,
+            columns: 6,
+            margin: 0
+        });
+
+        const hamburger = document.getElementById('hamburger');
+        const sidebar = document.getElementById('sidebar');
+
+        hamburger.addEventListener('click', () => {
+            sidebar.classList.toggle('open');
+            hamburger.classList.toggle('open');
+        });
+    </script>
+</body>
+
+</html>
--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -1,59 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Saud-Aslam - {{ $project['name'] }}</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="{{ asset('asset/css/landing.css') }}">
-</head>
-
-<body>
-    <div class="fixed inset-y-0 left-0 w-12 bg-white z-50 white-bar-container">
-        <!-- Bottom Section: Hicham Habchi -->
-        <div class="text-2xl font-bold pb-4" style="margin-top: 50px; margin-left: 10px">
-            <a href="/" class="white-bar text-black sidebar-text">Saud Aslam</a>
-        </div>
-        <!-- Top Section: MENU and Hamburger -->
-        <div class="flex flex-col items-center pt-4" style="margin-top: 250px">
-            <button id="hamburger" class="p-2 focus:outline-none flex flex-col items-center">
-                <span class="white-bar text-black text-sm font-medium mb-4">MENU</span>
-                <svg viewBox="0 0 24 24" class="w-6 h-6 text-black transition-colors duration-300" fill="none"
-                    xmlns="http://www.w3.org/2000/svg" transform="rotate(90)">
-                    <g>
-                        <path d="M4 18L20 18" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path>
-                        <path d="M4 12L20 12" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path>
-                        <path d="M4 6L20 6" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path>
-                    </g>
-                </svg>
-            </button>
-        </div>
-    </div>
-    <div id="sidebar" class="fixed inset-y-0 left-12 w-64 sidebar-bg text-white shadow-lg z-40 text-center">
-        <div class="flex flex-col h-full p-6">
-            <!-- Menu Items -->
-            <nav class="flex flex-col space-y-2">
-                <a href="/" class="py-2 px-4 text-lg hover:bg-gray-800 rounded transition duration-200">Home
-                    page</a>
-                <a href="#" class="py-2 px-4 text-lg hover:bg-gray-800 rounded transition duration-200">Resume</a>
-                <a href="#"
-                    class="py-2 px-4 text-lg hover:bg-gray-800 rounded transition duration-200">Portfolio</a>
-                <a href="#"
-                    class="py-2 px-4 text-lg hover:bg-gray-800 rounded transition duration-200">Instagram</a>
-                <a href="#"
-                    class="py-2 px-4 text-lg hover:bg-gray-800 rounded transition duration-200">Artbook</a>
-                <a href="#" class="py-2 px-4 text-lg hover:bg-gray-800 rounded transition duration-200">Prints</a>
-                <a href="#"
-                    class="py-2 px-4 text-lg hover:bg-gray-800 rounded transition duration-200">Contact</a>
-            </nav>
-        </div>
-    </div>
+@extends('layouts.front-header')
+@section('styles')
+    <style>
+        .cursor-pointer.img:hover {
+            color: #23c1ff;
+        }
+    </style>
+@endsection
+@section('content')
     <div class="content">
-        <button
-            class="absolute left-0 top-[400px] left-[15px] bg-gray-800 text-white p-3 rounded-full shadow hover:bg-gray-600">
-            &#10094;
-        </button>
+        <a class="absolute top-[400px] left-[70px] cursor-pointer">
+            <img src="{{ asset('left.svg') }}" class="w-6 h-6" />
+        </a>
         <div class="relative max-w-7xl mx-auto py-10 px-4 grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
             <!-- Left Arrow -->
             <!-- Left Side: Images -->
@@ -72,38 +29,63 @@
                 <p class="mt-4 text-sm text-gray-500">Date: {{ $project['ended_on'] }}</p>
 
                 <div class="mt-4 flex space-x-4">
-                    <button class="bg-blue-600 text-white px-4 py-2 rounded">Share</button>
-                    <button class="bg-red-600 text-white px-4 py-2 rounded">Pin</button>
-                    <button class="bg-blue-400 text-white px-4 py-2 rounded">Tweet</button>
+                    <svg class="w-7 h-7" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <title>Facebook-color</title>
+                            <desc>Created with Sketch.</desc>
+                            <defs> </defs>
+                            <g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g id="Color-" transform="translate(-200.000000, -160.000000)" fill="#4460A0">
+                                    <path
+                                        d="M225.638355,208 L202.649232,208 C201.185673,208 200,206.813592 200,205.350603 L200,162.649211 C200,161.18585 201.185859,160 202.649232,160 L245.350955,160 C246.813955,160 248,161.18585 248,162.649211 L248,205.350603 C248,206.813778 246.813769,208 245.350955,208 L233.119305,208 L233.119305,189.411755 L239.358521,189.411755 L240.292755,182.167586 L233.119305,182.167586 L233.119305,177.542641 C233.119305,175.445287 233.701712,174.01601 236.70929,174.01601 L240.545311,174.014333 L240.545311,167.535091 C239.881886,167.446808 237.604784,167.24957 234.955552,167.24957 C229.424834,167.24957 225.638355,170.625526 225.638355,176.825209 L225.638355,182.167586 L219.383122,182.167586 L219.383122,189.411755 L225.638355,189.411755 L225.638355,208 L225.638355,208 Z"
+                                        id="Facebook"> </path>
+                                </g>
+                            </g>
+                        </g>
+                    </svg>
+                    <svg class="w-7 h-7" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <title>Pinterest-color</title>
+                            <desc>Created with Sketch.</desc>
+                            <defs> </defs>
+                            <g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g id="Color-" transform="translate(-300.000000, -260.000000)" fill="#CC2127">
+                                    <path
+                                        d="M324.001411,260 C310.747575,260 300,270.744752 300,284.001411 C300,293.826072 305.910037,302.270594 314.368672,305.982007 C314.300935,304.308344 314.357382,302.293173 314.78356,300.469924 C315.246428,298.522491 317.871229,287.393897 317.871229,287.393897 C317.871229,287.393897 317.106368,285.861351 317.106368,283.59499 C317.106368,280.038808 319.169518,277.38296 321.73505,277.38296 C323.91674,277.38296 324.972306,279.022755 324.972306,280.987123 C324.972306,283.180102 323.572411,286.462515 322.852708,289.502205 C322.251543,292.050803 324.128418,294.125243 326.640325,294.125243 C331.187158,294.125243 334.249427,288.285765 334.249427,281.36532 C334.249427,276.10725 330.707356,272.170048 324.263891,272.170048 C316.985006,272.170048 312.449462,277.59746 312.449462,283.659905 C312.449462,285.754101 313.064738,287.227377 314.029988,288.367613 C314.475922,288.895396 314.535191,289.104251 314.374316,289.708238 C314.261422,290.145705 313.996119,291.21256 313.886047,291.633092 C313.725172,292.239901 313.23408,292.460046 312.686541,292.234256 C309.330746,290.865408 307.769977,287.193509 307.769977,283.064385 C307.769977,276.248368 313.519139,268.069148 324.921503,268.069148 C334.085729,268.069148 340.117128,274.704533 340.117128,281.819721 C340.117128,291.235138 334.884459,298.268478 327.165285,298.268478 C324.577174,298.268478 322.138649,296.868584 321.303228,295.279591 C321.303228,295.279591 319.908979,300.808608 319.615452,301.875463 C319.107426,303.724114 318.111131,305.575587 317.199506,307.014994 C319.358617,307.652849 321.63909,308 324.001411,308 C337.255248,308 348,297.255248 348,284.001411 C348,270.744752 337.255248,260 324.001411,260"
+                                        id="Pinterest"> </path>
+                                </g>
+                            </g>
+                        </g>
+                    </svg>
+                    <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100"
+                        viewBox="0 0 50 50">
+                        <path
+                            d="M 11 4 C 7.134 4 4 7.134 4 11 L 4 39 C 4 42.866 7.134 46 11 46 L 39 46 C 42.866 46 46 42.866 46 39 L 46 11 C 46 7.134 42.866 4 39 4 L 11 4 z M 13.085938 13 L 21.023438 13 L 26.660156 21.009766 L 33.5 13 L 36 13 L 27.789062 22.613281 L 37.914062 37 L 29.978516 37 L 23.4375 27.707031 L 15.5 37 L 13 37 L 22.308594 26.103516 L 13.085938 13 z M 16.914062 15 L 31.021484 35 L 34.085938 35 L 19.978516 15 L 16.914062 15 z">
+                        </path>
+                    </svg>
+                    <svg class="w-7 h-7" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path fill="#0A66C2"
+                                d="M12.225 12.225h-1.778V9.44c0-.664-.012-1.519-.925-1.519-.926 0-1.068.724-1.068 1.47v2.834H6.676V6.498h1.707v.783h.024c.348-.594.996-.95 1.684-.925 1.802 0 2.135 1.185 2.135 2.728l-.001 3.14zM4.67 5.715a1.037 1.037 0 01-1.032-1.031c0-.566.466-1.032 1.032-1.032.566 0 1.031.466 1.032 1.032 0 .566-.466 1.032-1.032 1.032zm.889 6.51h-1.78V6.498h1.78v5.727zM13.11 2H2.885A.88.88 0 002 2.866v10.268a.88.88 0 00.885.866h10.226a.882.882 0 00.889-.866V2.865a.88.88 0 00-.889-.864z">
+                            </path>
+                        </g>
+                    </svg>
                 </div>
             </div>
 
 
         </div>
         <!-- Right Arrow -->
-        <button class="absolute right-0 top-[400px] bg-gray-800 text-white p-3 rounded-full shadow hover:bg-gray-600">
-            &#10095;
-        </button>
+        <a class="absolute right-[40px] top-[400px] cursor-pointer">
+            <img src="{{ asset('right.svg') }}" class="w-6 h-6" />
+        </a>
     </div>
-
-    <script>
-        window.onscroll = function() {
-            var header = document.querySelector("header");
-            if (window.pageYOffset > 0) {
-                header.classList.add("sticky");
-            } else {
-                header.classList.remove("sticky");
-            }
-        };
-
-        const hamburger = document.getElementById('hamburger');
-        const sidebar = document.getElementById('sidebar');
-
-        hamburger.addEventListener('click', () => {
-            sidebar.classList.toggle('open');
-            hamburger.classList.toggle('open');
-        });
-    </script>
-</body>
-
-</html>
+@endsection
